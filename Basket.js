ad2534d905b53a6a9f7d55dbb98f2930d1f57287
konka84 <43616166+konka84@users.noreply.github.com>
2021-09-26 19:00:02 +0600

    Add files via upload

---
+++ Basket.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Basket(props) {
+  return (
+    
+    <aside className='block col-1'>
+    <h2>Cart Items</h2>
+      <div></div>
+      </aside>
+      );
+    }
